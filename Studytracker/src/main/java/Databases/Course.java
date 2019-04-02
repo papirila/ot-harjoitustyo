3ef5ee641b036959949d70765c84dab29a0c7819
papirila <pauliina.pirila@helsinki.fi>
2019-04-02 17:12:16 +0300

    muokattiin harjoitustyön ui:ta niin että suoritettuja kursseja voi lisätä listalle

---
+++ Studytracker/src/main/java/Databases/Course.java
@@ -0,0 +1,41 @@
+
+package Databases;
+
+
+public class Course {
+    
+    private int id;
+    private String name;
+    private int studyPoints;
+    private int grade;
+
+    public Course(int id, String name, int studyPoints, int grade) {
+        this.id = id;
+        this.name = name;
+        this.studyPoints = studyPoints;
+        this.grade = grade;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public int getGrade() {
+        return grade;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getStudyPoints() {
+        return studyPoints;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString(); 
+    }
+    
+    
+}
--- Studytracker/src/main/java/studytracker/UI/UI.java
+++ Studytracker/src/main/java/studytracker/UI/UI.java
@@ -1,10 +1,22 @@
 package studytracker.UI;
 
+import Databases.Course;
 import javafx.application.Application;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
@@ -13,22 +25,93 @@ public class UI extends Application {
     
     @Override
     public void start(Stage primaryStage) {
-        Button btn = new Button();
-        btn.setText("Say 'Hello World'");
-        btn.setOnAction(new EventHandler<ActionEvent>() {
-            
+        
+        GridPane grid = new GridPane();
+        HBox hbox = new HBox();
+        Label lb = new Label("Passed courses");
+        ObservableList<Course> data = FXCollections.observableArrayList();
+        
+        TextField addName = new TextField(); 
+        addName.setPromptText("Course name");
+        TextField addPoints = new TextField(); 
+        addPoints.setPromptText("Study points");
+        TextField addGrade = new TextField(); 
+        addGrade.setPromptText("Grade");
+        
+        
+        addPoints.textProperty().addListener(new ChangeListener<String>() {
             @Override
-            public void handle(ActionEvent event) {
-                System.out.println("Hello World!");
+            public void changed(ObservableValue<? extends String> observable, String oldValue, 
+                String newValue) {
+                if (!newValue.matches("\\d*")) {
+                    addPoints.setText(newValue.replaceAll("[^\\d]", ""));
+                }
+            }
+        });
+        addGrade.textProperty().addListener(new ChangeListener<String>() {
+            @Override
+            public void changed(ObservableValue<? extends String> observable, String oldValue, 
+                String newValue) {
+                if (!newValue.matches("\\d*")) {
+                    addGrade.setText(newValue.replaceAll("[^\\d]", ""));
+                }
+            }
+        });
+       
+        Button button = new Button("Add");
+        button.setOnAction(new EventHandler<ActionEvent>() {
+            @Override public void handle(ActionEvent e) {
+                System.out.println(addName.getText());
+                data.add(new Course(9999,
+                    addName.getText(),
+                    Integer.valueOf(addPoints.getText()),
+                    Integer.valueOf(addGrade.getText())
+                ));
+                addName.clear();
+                addPoints.clear();
+                addGrade.clear();
             }
         });
         
+        
+        ObservableList list = hbox.getChildren();  
+        list.addAll(addName, addPoints, addGrade, button);    
+        
+      
+        TableView passedCourses = new TableView();
+
+        TableColumn courseNameColumn = new TableColumn("Course name");
+        courseNameColumn.setMinWidth(200);
+        
+        courseNameColumn.setCellValueFactory(
+            new PropertyValueFactory<Course,String>("Name")
+        );
+        TableColumn studypointsColumn = new TableColumn("Study points");
+        studypointsColumn.setMinWidth(200);
+        
+        studypointsColumn.setCellValueFactory(
+            new PropertyValueFactory<Course,String>("studyPoints")
+        );
+        TableColumn gradeColumn = new TableColumn("Grade");
+        gradeColumn.setMinWidth(200);
+        
+        gradeColumn.setCellValueFactory(
+            new PropertyValueFactory<Course,String>("Grade")
+        );
+        passedCourses.setItems(data);
+        
+        passedCourses.getColumns().addAll(courseNameColumn, studypointsColumn, gradeColumn);
+        
+        grid.add(lb, 0, 0);
+        grid.add(passedCourses, 0, 1);
+        grid.add(hbox, 0, 2);
+        
         StackPane root = new StackPane();
-        root.getChildren().add(btn);
+        root.getChildren().add(grid);
         
-        Scene scene = new Scene(root, 300, 250);
+        Scene scene = new Scene(root, 1000, 1000);
         
-        primaryStage.setTitle("Hello World!");
+        primaryStage.setTitle("Studytracker");
         primaryStage.setScene(scene);
         primaryStage.show();
     }
